4e92b873cac035c7df85923ae25d426ba7008687
MrAnwar1808 <shaikanwar1808@gmail.com>
2025-03-04 11:44:52 +0530

    About us and  pro services

--- innovators/src/App.js
+++ innovators/src/App.js
@@ -1,9 +1,17 @@
 
+import AboutusPage from './Aboutus/Aboutus';
 import './App.css';
+// import WebDevelopmentPage from './Webdevelopment/webdevpage';
+// import Productcard from './Productsearch/Productcard';
 
 function App() {
   return (
     <div>
+
+      {/* <Productcard/> */}
+
+      {/* <WebDevelopmentPage/> */}
+      <AboutusPage/>
 
     </div>
   );
